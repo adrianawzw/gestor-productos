92e4093b2ea85ae1e496af3a918f6153c4fc07cd
Adriana Tapia <tapiaadrianalucia@gmail.com>
2026-02-10 23:13:36 -0500

    Fix: cors has changed

--- src/main/java/com/demo/productos/gestor_productos/config/CorsConfig.java
+++ src/main/java/com/demo/productos/gestor_productos/config/CorsConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+/* @Configuration
 public class CorsConfig {
 
   @Bean
@@ -23,4 +23,22 @@ public class CorsConfig {
       }
     };
   }
+}
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "http://localhost:4200",
+                            "https://gestor-productos-adri.netlify.app/"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+            }
+        };
+    }
 }
